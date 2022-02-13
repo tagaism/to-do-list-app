93fa9172299969f088f83944705e109f62f6e5ba
Bermet Tariel <bermet.tariel@gmail.com>
2022-02-12 23:47:21 -0600

    built basic design, created add to do component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import './App.css';
+import React from "react";
+import { Input, Fab, Box, Typography, Grid } from "@mui/material";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import "@fontsource/roboto/300.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      todo: [],
+    };
+  }
+
+  addTask = (e) => {
+    this.setState({ task: e.target.value });
+  };
+
+  addTodo = (e) => {
+    e.preventDefault();
+    this.setState({ todo: [this.state.task, ...this.state.todo] });
+  };
+
+  render() {
+    const { task, todo } = this.state;
+    console.log(todo);
+    return (
+      <div className="App">
+        <Box
+          sx={{
+            width: 800,
+            height: 800,
+            backgroundColor: "primary.light",
+          }}
+        >
+          <Input
+            type="text"
+            placeholder="Add new ..."
+            value={task}
+            onChange={this.addTask}
+          />
+          <Fab color="secondary" aria-label="add" onClick={this.addTodo}>
+            <AddIcon />
+          </Fab>
+
+          {todo.map((item) => {
+            return (
+              <div>
+                <Grid container sx={{ color: "text.primary"}}>
+                  <Grid item xs={5}>
+                    <Typography variant="h6">
+                      {item}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={7}>
+                    <DeleteIcon />
+                  </Grid>
+                </Grid>
+              </div>
+            );
+          })}
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default App;
