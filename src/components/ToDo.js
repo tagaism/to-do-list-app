d6c21781d4eff75119f0204d26cfa953d8bffb50
Bermet Tariel <bermet.tariel@gmail.com>
2022-02-13 22:48:36 -0600

    updated/added_editIcon_and_CheckboxIcon

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Typography, Grid } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from '@mui/icons-material/Edit';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 
 export default class ToDo extends Component {
   constructor() {
@@ -8,13 +10,15 @@ export default class ToDo extends Component {
   }
   render() {
     return (
-      <div onClick={() => this.props.deleteTodo(this.props.index)}>
+      <div>
         <Grid container sx={{ color: "text.primary" }}>
           <Grid item xs={5}>
             <Typography variant="h6">{this.props.item}</Typography>
           </Grid>
           <Grid item xs={7}>
-            <DeleteIcon />
+            <DeleteIcon onClick={() => this.props.deleteTodo(this.props.index)}/>
+            <EditIcon />
+            <CheckBoxIcon />
           </Grid>
         </Grid>
       </div>
